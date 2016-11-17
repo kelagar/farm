a7a9dddc8d50f09ae33c6c8c5caeda554b1f93ff
Anshuman Srivastava <Anshumansri@gmail.com>
2016-11-17 00:12:03 -0500

    WizardView
    Added a new view

--- farm/Controllers/HomeController.cs
+++ farm/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace farm.Controllers
         {
             return View();
         }
+
+        public ActionResult MyWizardView()
+        {
+            return View();
+        }
     }
 }
